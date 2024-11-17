25d396df9ed6dc419c27c3766edc6625088400cf
sasha-gerzhik2106 <167282821+sasha-gerzhik2106@users.noreply.github.com>
2024-11-17 08:55:50 +0000

    Deploying to gh-pages from @ sasha-gerzhik2106/goit-js-hw-12@7babc63a9766ba5aacdd17e53608e568aca4ed5e 🚀

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import{S as f,i as n}from"./assets/vendor-5ObWk2rO.js";(function(){const a=document.createElement("link").relList;if(a&&a.supports&&a.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&i(l)}).observe(document,{childList:!0,subtree:!0});function s(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=s(e);fetch(e.href,t)}})();const y="https://pixabay.com/api/",g="46829805-97afaea9a63466d4d0f01977e";async function h(r,a,s){const i=`${y}?key=${g}&q=${r}&page=${a}&per_page=${s}&image_type=photo&orientation=horizontal&safesearch=true`,e=await fetch(i);if(!e.ok)throw new Error(e.status);return e.json()}function b(r,a){const s=r.map(({webformatURL:i,largeImageURL:e,tags:t,likes:l,views:c,comments:o,downloads:d})=>`
+import{S as L,i as o}from"./assets/vendor-5ObWk2rO.js";(function(){const i=document.createElement("link").relList;if(i&&i.supports&&i.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const l of t.addedNodes)l.tagName==="LINK"&&l.rel==="modulepreload"&&s(l)}).observe(document,{childList:!0,subtree:!0});function a(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=a(e);fetch(e.href,t)}})();const w="https://pixabay.com/api/",_="46829805-97afaea9a63466d4d0f01977e";async function $(r,i,a){const s=`${w}?key=${_}&q=${r}&page=${i}&per_page=${a}&image_type=photo&orientation=horizontal&safesearch=true`,e=await fetch(s);if(!e.ok)throw new Error(e.status);return e.json()}function v(r,i){const a=r.map(({webformatURL:s,largeImageURL:e,tags:t,likes:l,views:g,comments:h,downloads:b})=>`
               <li class="gallery-list-item">
                   <a class="gallery_link" href="${e}">
-                      <img class="gallery_img" src="${i}" 
+                      <img class="gallery_img" src="${s}" 
                           alt="${t}" 
                           title="${t}" />
                       <ul class="statistics-list">
@@ -11,17 +11,17 @@ import{S as f,i as n}from"./assets/vendor-5ObWk2rO.js";(function(){const a=docum
                           </li>
                           <li class="statistics-item">
                               <p class="statistics-item_name">Views</p>
-                              <p class="statistics_result">${c}</p>
+                              <p class="statistics_result">${g}</p>
                           </li>
                           <li class="statistics-item">
                               <p class="statistics-item_name">Comments</p>
-                              <p class="statistics_result">${o}</p>
+                              <p class="statistics_result">${h}</p>
                           </li>
                           <li class="statistics-item">
                               <p class="statistics-item_name">Downloads</p>
-                              <p class="statistics_result">${d}</p>
+                              <p class="statistics_result">${b}</p>
                           </li>
                       </ul>
                   </a>
-              </li>`).join("");a.insertAdjacentHTML("beforeend",s)}const m=document.querySelector(".gallery-list"),_=document.querySelector("form"),p=document.querySelector(".loader");let L=new f(".gallery-list a",{});const u=document.querySelector(".load-more");function w(){let r="",s=1,i=null;async function e(t,l,c){p.style.display="block";try{if(i!==null&&l*c>=i)return n.warning({message:"We're sorry, but you've reached the end of search results."});const o=await h(t,l,c);o.hits.length===0?n.info({message:"Sorry, there are no images matching your search query. Please try again!"}):(i=o.totalHits,b(o.hits,m),L.refresh())}catch(o){n.error({message:o.message})}finally{p.style.display="none"}}_.addEventListener("submit",async t=>{if(t.preventDefault(),r=t.target.elements.choiceSearch.value.toLowerCase().trim(),r)m.innerHTML="";else{n.error({message:"Please enter a search word."});return}s=1,await e(r,s,15),u.style.visibility="visible"}),u.addEventListener("click",async()=>{u.style.visibility="hidden",s+=1,await e(r,s,15),u.style.visibility="visible"})}w();
+              </li>`).join("");i.insertAdjacentHTML("beforeend",a)}const p=document.querySelector(".gallery-list"),S=document.querySelector("form"),d=document.querySelector(".loader"),c=document.querySelector(".load-more"),q=new L(".gallery-list a",{});let n="";const f=15;let m=1,u=null;async function y(r,i,a){d.style.display="block";try{if(u!==null&&i*a>=u){o.warning({message:"We're sorry, but you've reached the end of search results."});return}const s=await $(r,i,a);if(s.hits.length===0){o.info({message:"Sorry, there are no images matching your search query. Please try again!"});return}u=s.totalHits,v(s.hits,p),q.refresh()}catch(s){o.error({message:s.message})}finally{d.style.display="none"}}S.addEventListener("submit",async r=>{if(r.preventDefault(),n=r.target.elements.choiceSearch.value.trim().toLowerCase(),!n){o.error({message:"Please enter a search word."});return}p.innerHTML="",m=1,u=null,await y(n,m,f),c.style.visibility="visible"});c.addEventListener("click",async()=>{m+=1,c.style.visibility="hidden",await y(n,m,f),c.style.visibility="visible"});
 //# sourceMappingURL=index.js.map
